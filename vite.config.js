1ebc7d2e46fa804d973716f7354be1464b769b4d
Nirina Rabeson <nirinarabeson@outlook.fr>
2023-08-09 14:55:15 +0000

    added test command

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
   },
-  base: "ethereum-wallet-generator",
+  base: "/"
 });
